417e1c2ddc3eb40a170efc9ae37e86fd4d0606cf
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-13 04:11:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,10 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false
     return true
+  if board[index] < 10
+    return true
+  else 
+    false
 end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
